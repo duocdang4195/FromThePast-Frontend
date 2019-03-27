a26d0081336b268d9adca732f6cfa7587bcbe340
Marry-Anne <thienhuong2003@gmail.com>
2019-03-27 21:04:07 +0700

    Add css/scss

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './components/views/Home.vue';
+import Emotion_view from './components/emotion/Emotion_view.vue';
+import login from './components/member/login.vue';
 
 Vue.use(Router);
 
@@ -11,6 +13,16 @@ export default new Router ({
 				path: '/',
 				name: 'home',
 				component: Home,
+			},
+			{
+				path: '/emotion',
+				name: 'emotion_view',
+				component: Emotion_view,
+			},
+			{
+				path: '/member',
+				name: 'login',
+				component: login,
 			},
 		]
 })
