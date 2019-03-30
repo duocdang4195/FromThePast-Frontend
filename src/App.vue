d164627cfb77c3573a4142d749e6678fa7ff8bc7
duocdang4195 <dangvanduoc1997@gmail.com>
2019-03-30 12:43:38 +0700

    fix css

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,11 @@ export default {
 </script>
 
 <style>
-
+  @import './assets/css/magnific-popup.css';
+  @import './assets/css/animate.min.css';
+  @import './assets/css/main.css';
+  @import './assets/css/responsive.css';
+  @import './assets/scss/app.scss';
+  /* @import './assets/css/themify-icons.css'; */
 </style>
 
