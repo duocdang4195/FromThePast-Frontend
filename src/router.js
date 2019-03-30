635de8210aa73e18bf9672f5831e6f31c0858290
duocdang4195 <dangvanduoc1997@gmail.com>
2019-03-30 13:42:37 +0700

    add authenticate

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { ifNotAuthenticated } from './plugins/authenticate-logic';
 import Home from './components/views/Home.vue';
 
 Vue.use(Router);
@@ -10,7 +11,8 @@ export default new Router ({
 			{
 				path: '/',
 				name: 'home',
-				component: Home,
+        component: Home,
+        beforeEnter: ifNotAuthenticated,
 			},
 		]
 })
