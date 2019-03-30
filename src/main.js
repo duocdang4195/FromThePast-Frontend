3217365f6f755a99e8a886227499032fc74a15ee
duocdang4195 <dangvanduoc1997@gmail.com>
2019-03-30 17:05:03 +0700

    del authenticate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
-import store from './store';
 import './plugins/vuetify';
 import 'bulma/css/bulma.css'
 import 'vuetify/dist/vuetify.min.css'
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { ifNotAuthenticated } from './plugins/authenticate-logic';
 import Home from './components/views/Home.vue';
 
 Vue.use(Router);
@@ -12,7 +11,6 @@ export default new Router ({
 				path: '/',
 				name: 'home',
         component: Home,
-        beforeEnter: ifNotAuthenticated,
 			},
 		]
 })
