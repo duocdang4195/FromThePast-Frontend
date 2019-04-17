d39cc23b63c30f0fba57b5082ec761bbd957520d
duoc1122 <dangvanduoc1997@gmail.com>
2019-04-17 17:36:07 +0700

    add menu side bar

---
+++ src/components/naviagtions/MenuSide.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar__child">
+      <div class="sidebar__child--elm">
+        my info
+      </div>
+      <div class="sidebar__child--elm">
+        my profit
+      </div>
+      <div class="sidebar__child--elm">
+        my writing
+      </div>
+      <div class="sidebar__child--elm">
+        my quotation
+      </div>
+      <div class="sidebar__child--elm">
+        my services
+      </div>
+      <div class="sidebar__child--elm">
+        my album
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .sidebar {
+    z-index: 998;
+    width: 300px;
+    height: 100%;
+    padding-top: 60px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transition: .5s;
+    background-color: rgba(0, 0, 0, 0.5);
+    &__child {
+      position: relative;
+      display: inline-block;
+      top: 70px;
+      white-space: nowrap;
+      text-transform: uppercase;
+      .sidebar__child--elm {
+        color: #b8b7ad;
+        font-size: 20px;
+        margin-left: 10%;
+        padding: 15px 10px;
+      }
+    }
+  }
+</style>
+
---
+++ src/components/naviagtions/NaviagtionTop.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="menu">
+    <div class="menu__logo">
+      <img src="http://tuquakhu.com/img/logo-dark.svg" alt="">
+    </div>
+    <div class="menu__child">
+      <div class="menu__child--elm">
+        <router-link :to="{ name: 'home'}">home</router-link>
+      </div>
+      <div class="menu__child--elm">
+        <router-link :to="{ name: 'Emotion_view'}">emotion</router-link>
+      </div>
+      <div class="menu__child--elm">
+        <router-link :to="{ name: 'service'}">service</router-link>
+      </div>
+      <div class="menu__child--elm">
+        <router-link :to="{ name: 'Emotion_write'}">become a writer</router-link>
+      </div>
+      <div class="menu__child--elm">
+        <router-link :to="{ name: 'about_list'}">about</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .menu {
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    padding: 0 8%;
+    box-shadow: 1px 3px 3px rgba(180, 180, 180, 0.3);
+    background-color: rgba(255, 255, 255, 0.9);
+    height: 70px;
+    &__logo {
+      display: inline-block;
+      line-height: 70px;
+      width: 140px;
+    }
+    &__child {
+      float: right;
+      .menu__child--elm {
+        font-size: 18px;
+        display: inline-block;
+        padding: 0 15px;
+        line-height: 70px;
+        text-transform: uppercase;
+        letter-spacing: 3.5;
+      }
+    }
+  }
+</style>
+
