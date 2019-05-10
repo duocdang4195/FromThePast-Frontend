847652a3eebbc70c2b6290dd76c205db71cf1a67
duocdang4195 <dangvanduoc1997@gmail.com>
2019-05-10 23:17:58 +0700

    resolve conflict

--- src/router.js
+++ src/router.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import Vue from "vue";
-import Router from "vue-router";
-import HomeRoot from "./components/HomeRoot.vue";
-import Home from "./components/views/Home.vue";
-import main from "./components/views/main.vue";
-import AuthenticateForm from "./components/authenticate/AuthenticateForm.vue";
-import about_list from "./components/about/about_list.vue";
-import AboutView from "./components/about/AboutView.vue";
-import my_info from "./components/my_page/my_info.vue";
-import my_profit from "./components/my_page/my_profit.vue";
-import my_quotation from "./components/my_page/my_quotation.vue";
-import my_writing from "./components/my_page/my_writing.vue";
-import Emotion_write from "./components/emotion/Emotion_write.vue";
-import Emotion_list from "./components/emotion/Emotion_list.vue";
-import Emotion_view from "./components/emotion/Emotion_view.vue";
-import service from "./components/service/service.vue";
-import become_writer from "./components/service/become_writer.vue";
-import search from "./components/views/search.vue";
-import checkout from '@/components/checkout/checkout.vue'
-=======
 import Vue from 'vue';
 import Router from 'vue-router';
 import HomeRoot from './components/HomeRoot.vue'
@@ -45,7 +24,6 @@ import order_detail from './components/checkout/order_detail.vue';
 import finish_booking from './components/checkout/finish_booking.vue';
 import service from './components/service/service.vue';
 import become_writer from './components/service/become_writer.vue';
->>>>>>> ffd7673a50d209b93d6ed12457b7c790e78bb8a8
 
 import {
   ifNotAuthenticated,
@@ -54,85 +32,6 @@ import {
 
 Vue.use(Router);
 
-<<<<<<< HEAD
-export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/home",
-      component: HomeRoot,
-      children: [
-        {
-          path: "",
-          name: "home_root",
-          component: main
-        },
-        {
-          path: "/about_list",
-          name: "about_list",
-          component: about_list
-        },
-        {
-          path: "/about_view/:id",
-          name: "about_view",
-          component: AboutView
-        },
-        {
-          path: "/my_info",
-          name: "my_info",
-          component: my_info
-        },
-        {
-          path: "/my_profit",
-          name: "my_profit",
-          component: my_profit
-        },
-        {
-          path: "/my_quotation",
-          name: "my_quotation",
-          component: my_quotation
-        },
-        {
-          path: "/my_writing",
-          name: "my_writing",
-          component: my_writing
-        },
-        {
-          path: "/Emotion_write",
-          name: "Emotion_write",
-          component: Emotion_write
-        },
-        {
-          path: "/Emotion_list",
-          name: "Emotion_list",
-          component: Emotion_list
-        },
-        {
-          path: "/Emotion_view/:id",
-          name: "Emotion_view",
-          component: Emotion_view
-        },
-        {
-          path: "/service",
-          name: "service",
-          component: service
-        },
-        {
-          path: "/search",
-          name: "search",
-          component: search
-        },
-        {
-          path: "/become_writer",
-          name: "become_writer",
-          component: become_writer
-        },
-        {
-          path: "/checkout",
-          name: "checkout",
-          component: checkout
-        }
-=======
 export default new Router ({
 	mode: 'history',
 	routes: [
@@ -245,7 +144,6 @@ export default new Router ({
 					name: 'setup_pass',
 					component: setup_pass,
 				},
->>>>>>> ffd7673a50d209b93d6ed12457b7c790e78bb8a8
       ],
       beforeEnter: ifAuthenticated
     },
