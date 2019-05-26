3672d2c025f199cc07b45bd8242c4570fa6027e5
duocdang4195 <dangvanduoc1997@gmail.com>
2019-05-27 00:53:32 +0700

    add policy

--- src/components/checkout/PolicyInsurence.vue
+++ src/components/checkout/PolicyInsurence.vue
@@ -13,6 +13,60 @@
               <br> Phí dịch vụ = Phí thời gian + Phí lưu trữ + Phí vận chuyển </p>
           </div>
         </div>
+
+        <div class="mr-about-flexbox">
+          <div class="mr-cntnBox">
+            <h4>PHÍ THỜI GIAN (Time Price) </h4>
+            <p>Chúng tôi tin rằng, con người chúng ta luôn có muôn vàn những loại cảm xúc khác nhau trong mỗi ngày, điều đó được đặt một cách vô tình hoặc hữu ý vào những đồ vật hoặc sự việc trong trí nhớ. Việc quản trị cảm xúc với nguồn xúc cảm hàng ngày là một điều không dễ dàng, đôi khi theo thời gian ta đánh mất hoặc quên đi những xúc cảm đáng trân trọng đó.  </p>
+            <p>Chúng ta sẽ cùng tìm cách nhắc nhờ nó trở về như là một món quà cho chính bản thân và những người bạn chọn lựa. Vậy nên, chúng tôi đề cao việc bạn gửi cảm xúc của mình cho chúng tôi càng lâu càng tốt, để giá trị khi nhận về lại sẽ là cao nhất và dễ thăng hoa nhất khi được gợi nhớ về. </p>
+            <p>Với tôn chỉ phục vụ trên, chúng tôi sẽ tính giá thời gian càng lâu giá đơn hàng của bạn càng ít phí cho đến miễn phí: </p>
+            <p><small>Hinh anh minh hoa</small></p>
+          </div>
+        </div>
+
+        <div class="mr-about-flexbox">
+          <div class="mr-cntnBox">
+            <h4>CHI PHÍ LƯU TRỮ (Security price): </h4>
+            <p>Việc được bạn lựa chọn Pasness® lưu giữ cảm xúc của bạn làm chúng tôi cảm thấy rất tự hào và hiểu rằng đó là một trọng trách vô cùng lớn cần được thực hiện với sự cam kết và đảm bảo cao nhất, luôn phải an toàn, bảo mật và trung thực. Việc lưu trữ sẽ được hoàn toàn bảo mật (đặc biệt là với thư viết tay được nhận từ bạn). Tất cả các thư in (bài viết, hoặc bạn tự viết bài gửi về) sẽ được in ra giấy và bảo quản ngay sau khi đơn hàng được đặt thành công. Chúng tôi sẽ lưu giữ tại Trung tâm quản lý tài sản khách hàng được an toàn cho đến ngày hẹn giao. </p>
+            <p>Việc lưu trữ này được tính phí tăng chậm dần đều, các khoản phí này được dùng để bảo trì cơ sở vật chất và lương cho đội ngũ quản lý tài sản của khách hàng theo thời gian. Vui lòng tham khảo cách tính phí lưu trữ dưới đây: </p>
+            <p><small>Hinh anh minh hoa</small></p>
+          </div>
+        </div>
+
+
+        <div class="mr-about-flexbox">
+          <div class="mr-cntnBox">
+            <h4>CHI PHÍ VẬN CHUYỂN/ GIAO NHẬN (Move Price) </h4>
+            <p>Chúng tôi cố gắng giao thư đến tận tay người nhận theo nội dung bạn đặt. Thách thức của chúng tôi là giao cho người nhận đúng thời gian để cảm xúc nhận được là giá trị nhất. </p>
+            <p>Chi phí vận chuyển bao gồm các chi phí cho việc chuyển thư đến Trung tâm quản lý tài sản Khách hàng để lưu trữ bảo mật, an toàn, và chi phí nhận thư và gửi thư theo lịch, địa chỉ được bạn sắp đặt cùng với Pasness®.  </p>
+            <p>Với mong muốn phục vụ bạn tốt nhất, chúng tôi hợp tác với đối tác Google Map để có thể định vị tốt nhất vị trí của bạn cung cấp và đối chiếu với các địa điểm gần nhất mà chúng tôi có thể đến với bạn để có lịch trình hợp lý với chi phí vận chuyển tốt nhất cho bạn. Vui lòng tham khảo cách tính phí vận chuyển/ giao nhận dưới đây: </p>
+            <p><small>Hinh anh minh hoa</small></p>
+          </div>
+        </div>
+
+        <div class="mr-about-flexbox">
+          <div class="mr-cntnBox">
+            <h4>CAM KẾT VÀ SỰ ĐẢM BẢO CỦA CHÚNG TÔI (Service insurance commitment) </h4>
+            <p>Chúng tôi biết rằng bạn sẽ cảm thấy hơi lạ lẫm một chút với cách tính này, tuy nhiên, hãy an tâm rằng phí dịch vụ của Pasness® luôn tốt nhất, được hiển thị rõ ràng trên mỗi đơn hàng bạn yêu cầu. </p>
+            <p>Với tôn chỉ cao nhất của dịch vụ là Tôn vinh cảm xúc con người để dẫn họ đến thành công và hạnh phúc. Pasness® cam kết đảm bảo tính thực thi và trách nhiệm của dịch vụ mà chúng tôi thi hành. </p>
+            <p>Mỗi đơn hàng bạn đặt thành công được mặc định cam kết 2 điều: </p>
+            <ul>
+              <li>Bạn mặc nhiên ký hợp đồng bảo vệ khách hàng mà From The PAST® đã ký với đối tác thứ 3 là những công ty Luật đứng trên vai trò là Luật sư bảo vệ quyền lợi riêng cho Khách hàng trong mỗi đơn hàng bạn giao dịch thành công với Pasness®. Danh sách bạn có thể lựa chọn đơn vị Luật sư đại diện cho mình trong danh mục My Info của bạn. Vui lòng xem thêm về thoả ước Luật sư cho Khách hàng tại đây. </li>
+              <li>Mỗi đơn hàng giao dịch thành công, chúng tôi cam kết bồi hoàn gấp 1000 lần so với Chi phí Lưu trữ (Security price). Khoản cam kết này được link trực tiếp đến hợp đồng bảo vệ Khách hàng của đơn vị thứ 3 là nhóm Luật sư của Khách hàng để đảm bảo thông tin được minh bạch. Mức “Service insurance commitment” cũng được thể hiện rõ trong mỗi đơn hàng bạn yêu cầu. </li>
+            </ul>
+            <p>Hãy tận hưởng Pasness® và dịch vụ của From The PAST® với cam kết phục vụ lợi ích cho bạn ngày một tốt hơn, và đáng tin tưởng hơn. Hãy cho phép Pasness.com trở thành một phần trong tâm hồn bạn, được cùng bạn tôn vinh cảm xúc và đạt đến giá trị thăng hoa của thành công và hạnh phúc. </p>
+          </div>
+        </div>
+
+
+        <div class="mr-about-flexbox">
+          <div class="mr-cntnBox">
+            <p>Trân trọng, </p>
+            <strong>Đội ngũ Pasness.com </strong>
+          </div>
+        </div>
+
+
       </div>
     </div>
   </div>
@@ -26,6 +80,8 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+
 .mr-about-view-bgr {
   position: relative;
   display: inline-block;
@@ -76,6 +132,10 @@ export default {
   display: flex;
   width: 100%;
   flex-wrap: wrap;
+}
+.mr-about-flexbox ul li {
+  margin-bottom: 10px;
+  list-style-type: decimal;
 }
 .mr-about-flexbox > div {
   position: relative;
